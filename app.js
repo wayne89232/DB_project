a74a4cddc190fd5fb4d5a97dbe66e90d6f8b1fde
wei <b01705015@ntu.edu.tw>
2014-12-30 23:25:47 +0800

    add some functions

--- app.js
+++ app.js
@@ -50,9 +50,7 @@ if (env === 'production') {
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 
-// JSON API
-app.get('/api/name', api.name);
-app.get('/api/name2', api.name2);
+
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,35 @@
-/*
- * Serve JSON to our AngularJS client
- */
 
-exports.name = function (req, res) {
-  res.json({
-    name: 'Wei'
-  });
-};
 
-exports.name2 = function (req, res) {
-  res.json({
-    name: 'Wei2'
-  });
-};
+var League = require('../models').League;
+var Team = require('../models').Team;
+
+
+
+exports.add_league = function(req, res){
+	League.create(req.body).success(function(league){
+		console.log("Success on adding league" + league.league_name);
+	});
+}
+
+exports.add_team = function(req, res){
+	Team.create(req.body).success(function(team){
+		//create relations with league    (league_team)
+		if(req.body.league == null){
+			// League_team.create({
+			// 	team_id: team.team_id,
+			// 	league_id: req.body.league 
+			// }).success(function(lt){
+			// 	console.log("Success on adding team" + team.team_name + " to league "+lt.league_id);
+			// }).error(function(err){
+			// 	console.log(err+": fail on adding team, maybe no league there");
+			// });
+		}
+		else{
+			console.log("Success on adding team" + team.team_name + " not in any league");
+		}
+	});
+	res.json({
+		team: req.body.name,
+		league: req.body.league
+	});
+}
