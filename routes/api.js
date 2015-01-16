420537e8db42d83300517ea33294bf06686e589d
DSW <dsw41923@gmail.com>
2015-01-16 15:26:22 +0800

    ADD TEAM & PLAYER FUNCTION

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 
-
 var League = require('../models').League;
 var Team = require('../models').Team;
 var City = require('../models').City;
+var School = require('../models').School;
+var Player = require('../models').Player;
 var _ = require('underscore');
 
 
@@ -33,24 +34,45 @@ exports.add_league = function(req, res){
 }
 
 exports.add_team = function(req, res){
-	Team.create(req.body).then(function(team){
-		//create relations with league    (league_team)
-		if(req.body.league == null){
-			// League_team.create({
-			// 	team_id: team.team_id,
-			// 	league_id: req.body.league 
-			// }).then(function(lt){
-			// 	console.log("Success on adding team" + team.team_name + " to league "+lt.league_id);
-			// }).error(function(err){
-			// 	console.log(err+": fail on adding team, maybe no league there");
-			// });
+	School.find({ where: { school_name: req.body.school } }).then(function(result){
+		if(_.size(result) == 0){
+			School.create({ school_name: req.body.school, city_id: req.body.city}).then(function(result2){
+				Team.create({
+					team_name: req.body.name,
+					school_id: result2.school_id
+				}).then(function(team){
+					res.json({ msg: "Success on adding team " + team.team_name });
+				});
+			});
 		}
 		else{
-			console.log("Success on adding team" + team.team_name + " not in any league");
+			Team.create({
+				team_name: req.body.name,
+				school_id: result.school_id
+			}).then(function(team){
+				res.json({ msg: "Success on adding team " + team.team_name });
+			});			
 		}
 	});
-	res.json({
-		team: req.body.name,
-		league: req.body.league
+}
+
+exports.add_player = function(req, res){
+	Team.find({ where: { team_name: req.body.team } }).then(function(result){
+		if(_.size(result) == 0){
+				Player.create({
+					player_name: req.body.name,
+    				team_id: result.city_id,
+	    			position: req.body.position,
+    				age: req.body.age,
+					height: req.body.height,
+    				weight: req.body.weight,
+    				batting: req.body.batting
+				}).then(function(player){
+					res.json({ msg: "Success on adding player " + player.player_name });
+				});
+		}
+		else{
+				res.json({ msg: "Fail on adding player " + player.player_name });
+		}
 	});
 }
