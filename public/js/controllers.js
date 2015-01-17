5ac4ea55efff5c6822d778b6d803530543654391
DSW <dsw41923@gmail.com>
2015-01-17 11:35:09 +0800

    I DONT KNOW

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('myApp.controllers', ['ngRoute']).controller('AppCtrl', function 
         $scope.leagues = leagues.msg;
     });
     $scope.add_league = function(){
-        if($scope.name != null && $scope.city != null && $scope.yaer != null ){
+        if($scope.name != null && $scope.city != null && $scope.year != null ){
             var data = {
                 name: $scope.name, 
                 city: $scope.city,
